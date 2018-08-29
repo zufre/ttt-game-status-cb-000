94ad3bc510b67c7fcbfa85e1465616842662832a
Cedric David <cedric.david@btopenworld.com>
2018-08-29 00:04:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], 
+  [0,1,2],
   [3,4,5],
   [6,7,8],
   [0,4,6],
@@ -13,4 +13,3 @@ WIN_COMBINATIONS = [
   [2,6,8],
   [0,4,8],
   [6,4,2],
-  
